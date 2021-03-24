97eca9bc5ead247602c444aa3f4a8c3e2dae53f1
nikitakuklev <spontaneouscoder@gmail.com>
2021-03-23 22:13:51 -0500

    Autoset number of cores

--- __init__.py
+++ __init__.py
@@ -7,14 +7,22 @@ __author__  = "Nikita Kuklev"
 
 # General imports - without these, most things will not work
 import sys
+import os
 
-import numpy as np
-import pandas as pd
 import logging
 
 logging.basicConfig(format='[%(asctime)s] {%(name)s:%(funcName)s:%(lineno)d} %(levelname)s - %(message)s',
                     level=logging.INFO,
                     stream=sys.stdout)
+
+# Detect number of cores and limit to 8
+N_CORES_MAX = 8
+n_cores = os.cpu_count()
+os.environ["NUMEXPR_MAX_THREADS"] = str(n_cores if n_cores <= N_CORES_MAX else N_CORES_MAX)
+
+
+
+import pyIOTA.lattice as lat
 
 #%(module)s
 #%(pathname)s
