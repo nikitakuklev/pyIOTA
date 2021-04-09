7d3a24cb59098147ba6cd204494558e2dd6479ac
nikitakuklev <spontaneouscoder@gmail.com>
2021-04-08 21:17:31 -0500

    Packaging will continue until morale improves

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+import pyiota
+
+setup(
+   name='pyiota',
+   version=pyiota.__version__,
+   description='',
+   author='Nikita Kuklev',
+   author_email='',
+   license="GNU General Public License v3 or later (GPLv3+)",
+   url="https://github.com/nikitakuklev/pyIOTA",
+   packages=find_packages(),
+)
