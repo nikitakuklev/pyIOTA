e622ada7ffd5f41eaab4c66808a8cea10649232d
nikitakuklev <spontaneouscoder@gmail.com>
2021-07-05 10:47:04 -0500

    Default dask folder is now scratch - should speed up writes

--- pyiota/util/config.py
+++ pyiota/util/config.py
@@ -3,7 +3,8 @@ import pathlib
 DASK_SCHEDULER_PORT = 10421
 DASK_SCHEDULER_ADDRESS = f'tcp://localhost:{DASK_SCHEDULER_PORT}'
 
-DASK_DEFAULT_WORK_FOLDER = pathlib.PurePosixPath('/home/nkuklev/studies/dask_tasks/')
+#DASK_DEFAULT_WORK_FOLDER = pathlib.PurePosixPath('/home/nkuklev/studies/dask_tasks/')
+DASK_DEFAULT_WORK_FOLDER = pathlib.PurePosixPath('/home/nkuklev/scratch/dask_tasks/')
 
 ELEGANT_MPI_MODULE = 'openmpi/1.10+intel-16.0' #mpich
 ELEGANT_MPI_ARGS = '--map-by core:nooversubscribe'
