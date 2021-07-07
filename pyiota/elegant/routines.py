63e0ef3b6f9d5085b9d56d75caf0fccf22c86f62
nikitakuklev <spontaneouscoder@gmail.com>
2021-07-07 14:45:55 -0500

    Add FMA template for elegant

--- pyiota/elegant/routines.py
+++ pyiota/elegant/routines.py
@@ -12,8 +12,12 @@ from pathlib import Path, PurePath, PurePosixPath
 from ocelot import Marker
 
 
-def standard_sim_job(work_folder: PurePath, lattice_options: Dict = None, add_random_id: bool = True,
-                     parameter_file_map: Dict = None, **kwargs):
+def standard_sim_job(work_folder: PurePath,
+                     lattice_options: Dict = None,
+                     task_options: Dict = None,
+                     add_random_id: bool = True,
+                     parameter_file_map: Dict = None,
+                     **kwargs):
     assert isinstance(work_folder, PurePath)
 
     defaults = {'label': 'test_label', 'lat_label': 'lat_label'}
@@ -34,6 +38,7 @@ def standard_sim_job(work_folder: PurePath, lattice_options: Dict = None, add_ra
 
     etask = ElegantSimJob(label=label,
                           lattice_options=lattice_options,
+                          task_options=task_options,
                           work_folder=work_folder,
                           data_folder=None,
                           run_subfolder=run_subfolder,
@@ -69,28 +74,48 @@ def template_task_track_single(box: LatticeContainer, t: Task, **kwargs):
 
 
 def template_task_track_watchpoint(box: LatticeContainer, t: Task, **kwargs):
-    """ Task template for tracking single particle at origin watchpoint over many turns """
-    assert len(kwargs) <= 3
+    """ Task template for tracking over many turns """
+    assert len(kwargs) <= 6
+    keys = set(kwargs.keys())
+    critical_args = {'n_turns'}
+    missing = critical_args.difference(keys)
+    if missing:
+        raise Exception(f'Missing critical keys: ({missing})')
+
     orbit = kwargs.get('orbit', 'closed_offset')
     n_turns = kwargs.get('n_turns', 1)
     sdds_beam = kwargs.get('sdds_beam', None)
-    assert n_turns >= 1
+    bunch_dict = kwargs.get('bunch_dict', None)
+    create_file = kwargs.get('create_file', False)
+    rf_mode = kwargs.get('rf_mode', None)
 
-    # Coordinates will be extracted from centroid file, no need for watchpoints
     t.setup_run_setup(p=box.pc, beamline='iota', rootname='test')
-    # assert isinstance(box.sequence[0], Marker)
-    # eid = box.lattice.sequence[0].id
-    # t.action_insert_watch(name=f'COORDS_{eid}', label=eid, mode='coordinates',
-    #                       start_pass=0, end_pass=-1, loc_name=eid)
+    assert isinstance(box.sequence[0], Marker)
+
     t.setup_run_control(n_passes=n_turns)
-    if sdds_beam:
-        # Input single particle via sdds
-        t.setup_sdds_beam(path=sdds_beam)
-    else:
-        # Just make one on reference orbit
-        t.setup_bunched_beam()
-    t.action_twiss(create_file=True, output_at_each_step=1)
+    t.action_twiss(create_file=True, full=True)
+    t.action_twiss(create_file=True, full=True, ext='twi2', output_at_each_step=1)
+
     t.setup_closed_orbit(verbosity=1)
+
+    # RF
+    if rf_mode == 'rf':
+        t.setup_rf(total_voltage=340.0, each_step=True)
+    elif isinstance(rf_mode, (float, int)):
+        t.setup_rf(total_voltage=rf_mode, each_step=True)
+    elif rf_mode is None:
+        pass
+
+    if sdds_beam is not None:
+        # Input particles via sdds
+        t.setup_sdds_beam(path=sdds_beam)
+    elif bunch_dict is not None:
+        # Make a bunch using elegant
+        t.setup_bunched_beam(mode='custom_bunch', bunch_dict=bunch_dict, create_file=create_file)
+    else:
+        # Just make one particle on reference orbit
+        t.setup_bunched_beam()
+
     t.action_track(orbit=orbit)
 
 
@@ -102,15 +127,57 @@ def template_task_closed_orbit(box: LatticeContainer, t: Task, **kwargs):
     t.setup_run_setup(p=box.pc, beamline='iota', rootname='test')
     t.setup_run_control()
     t.action_twiss(create_file=True, output_at_each_step=1)
-    t.setup_bunched_beam()
+    t.setup_bunched_beam(mode='empty')
     t.setup_closed_orbit(verbosity=1, create_file=True)
     t.action_track(orbit='default')
     return t
 
 
-def template_fma_setup(box: LatticeContainer,
-                       t: Task,
-                       **kwargs):
+def template_task_frequency_map(box: LatticeContainer, t: Task, **kwargs):
+    """ Task template for frequency map """
+    keys = set(kwargs.keys())
+    critical_args = {'n_turns', 'nx', 'ny', 'xmax', 'ymax'}
+    missing = critical_args.difference(keys)
+    if missing:
+        raise Exception(f'Missing critical keys: ({missing})')
+    rf_mode = kwargs.get('rf_mode', None)
+    chrom = kwargs.get('chrom', None)
+    n_turns = kwargs.get('n_turns', 1)
+    assert n_turns >= 1
+
+    t.setup_global_settings()
+    t.setup_run_setup(p=box.pc, beamline='iota', rootname='test', load_balance=True)
+    t.setup_run_control(n_passes=n_turns)
+
+    if chrom is not None:
+        bothnan = np.isnan(chrom[1]) and np.isnan(chrom[2])
+        notnan = not np.isnan(chrom[1]) and not np.isnan(chrom[1])
+        assert bothnan or notnan
+        if notnan:
+            t.setup_chromaticity(families=chrom[0], dnux_dp=chrom[1], dnuy_dp=chrom[2],
+                                 change_defined_values=1, n_iterations=10, exit_on_failure=1)
+
+    # Twiss on clean modified lattice
+    t.action_twiss(create_file=True, full=True)
+    t.action_twiss(create_file=True, ext='twi2', output_at_each_step=1, full=True)
+
+    t.setup_closed_orbit(verbosity=1)
+
+    # RF
+    if rf_mode == 'rf':
+        t.setup_rf(total_voltage=340.0, each_step=True)
+    elif isinstance(rf_mode, (float, int)):
+        t.setup_rf(total_voltage=rf_mode, each_step=True)
+    elif rf_mode is None:
+        pass
+
+    t.setup_bunched_beam(mode='empty')
+    t.action_frequency_map(quadratic_spacing=False, include_changes=True, full_grid_output=True,
+                           x=kwargs['xmax'], nX=kwargs['nx'], y=kwargs['ymax'], nY=kwargs['ny'],
+                           xmin=kwargs['xmin'], ymin=kwargs['ymin'])
+
+
+def template_fma_setup(box: LatticeContainer, t: Task, **kwargs):
     assert 'oc' in kwargs and kwargs['oc']
     keys = set(kwargs.keys())
     critical_args = {'beamline', 'turns', 'rf_mode', 'errors'}
