e3faf63ee7b0691a14e100dc475880debd16dc31
nikitakuklev <spontaneouscoder@gmail.com>
2021-07-12 14:11:22 -0500

    fix twiss elegant logic

--- pyiota/elegant/utils.py
+++ pyiota/elegant/utils.py
@@ -1,4 +1,4 @@
-__all__ = ['compare_twiss_to_box']
+__all__ = ['compare_twiss_to_box', 'merge_twiss_with_box']
 
 import gc
 __import__('tables')  # <-- import PyTables; __import__ so that linters don't complain
@@ -6,15 +6,29 @@ import h5py
 import numpy as np
 import pandas as pd
 
+def merge_twiss_with_box(box, twi):
+    df_t = box.twiss_df()
+    df_t['name'] = df_t['name'].shift(1).str.lower()
+    twi['ElementName'] = twi['ElementName'].str.lower()
+
+    df_tc = df_t.drop_duplicates('name')
+    twic = twi.drop_duplicates('ElementName')
+
+    df_m = df_tc.merge(twic, right_on=['ElementName'], left_on=['name'])
+    assert all(np.isclose(df_m.s_x, df_m.s_y))
+    return df_m
 
 def compare_twiss_to_box(box, twi: pd.DataFrame):
     df_t = box.twiss_df()
     df_t['name'] = df_t['name'].shift(1).str.lower()
     twi['ElementName'] = twi['ElementName'].str.lower()
-    df_m = df_t.merge(twi, right_on=['ElementName'], left_on=['name'])
+    df_tc = df_t.drop_duplicates('name')
+    twic = twi.drop_duplicates('ElementName')
+    df_m = df_tc.merge(twic, right_on=['ElementName'], left_on=['name'])
+    assert all(np.isclose(df_m.s_x, df_m.s_y))
     pairs = [('beta_x', 'betax'), ('mux', 'psix'), ('Dx', 'etax'), ('Dxp', 'etaxp'), ('beta_y', 'betay'),
              ('muy', 'psiy'), ('Dy', 'etay'), ('Dyp', 'etayp'), ]
-    assert len(df_m) == min(len(twi)-2, len(df_t)-2)
+    assert len(df_m) >= 1
     for p in pairs:
         v = (df_m.loc[:, p[0]] - df_m.loc[:, p[1]]).abs().max()
         if v > 1e-8:
