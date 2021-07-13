f4a61808cef7b8408e7ec360950385029a508bbc
nikitakuklev <spontaneouscoder@gmail.com>
2021-07-12 22:16:00 -0500

    mpich is faster

--- pyiota/util/config.py
+++ pyiota/util/config.py
@@ -6,7 +6,8 @@ DASK_SCHEDULER_ADDRESS = f'tcp://localhost:{DASK_SCHEDULER_PORT}'
 #DASK_DEFAULT_WORK_FOLDER = pathlib.PurePosixPath('/home/nkuklev/studies/dask_tasks/')
 DASK_DEFAULT_WORK_FOLDER = pathlib.PurePosixPath('/home/nkuklev/scratch/dask_tasks/')
 
-ELEGANT_MPI_MODULE = 'openmpi/1.10+intel-16.0' #mpich
+#ELEGANT_MPI_MODULE = 'openmpi/1.10+intel-16.0'
+ELEGANT_MPI_MODULE = 'mpich/3.2'
 ELEGANT_MPI_ARGS = '--map-by core:nooversubscribe'
 
 
