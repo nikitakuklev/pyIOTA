722fa02319fa3d4bd15d87205dae65e3b93c78dc
nikitakuklev <spontaneouscoder@gmail.com>
2021-07-28 06:35:26 -0500

    Version bump - defense draft

--- pyiota/__init__.py
+++ pyiota/__init__.py
@@ -2,7 +2,7 @@
 pyIOTA accelerator control and analysis framework
 """
 
-__version__ = '0.8.0'
+__version__ = '0.9.0'
 __author__ = "Nikita Kuklev"
 
 # General imports - without these, most things will not work
