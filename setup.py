4004982933bfad55745b07e72c49be70cb2803e6
nikitakuklev <spontaneouscoder@gmail.com>
2023-04-07 10:46:24 -0500

    Add better setup files

--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
 from setuptools import setup, find_packages
 import pyiota
 
+
+INSTALL_REQUIRES = [
+    'numpy>=1.13.3',
+    'pandas>=1.0',
+    'pydantic>=1.10',
+    'scipy',
+
+]
+EXTRAS_REQUIRES = {
+    "develop": [
+        "pytest>=6.0",
+    ]
+}
+LICENSE = "GNU General Public License v3 or later (GPLv3+)"
+DESCRIPTION = 'Toolbox for computational and experimental accelerator physics'
+CLASSIFIERS = [
+    "Programming Language :: Python :: 3.9",
+    "Operating System :: OS Independent",
+    "Topic :: Scientific/Engineering",
+    "Intended Audience :: Science/Research",
+]
+
 setup(
    name='pyiota',
    version=pyiota.__version__,
-   description='',
    author='Nikita Kuklev',
    author_email='',
-   license="GNU General Public License v3 or later (GPLv3+)",
+   description=DESCRIPTION,
+   license=LICENSE,
    url="https://github.com/nikitakuklev/pyIOTA",
    packages=find_packages(),
+   platforms="any",
+   install_requires=INSTALL_REQUIRES,
+   python_requires=">=3.9",
+   extras_require=EXTRAS_REQUIRES
 )
