130d78763cccc3ec9d02c0f26315ed60f0148eba
nikitakuklev <spontaneouscoder@gmail.com>
2023-05-31 16:39:32 -0500

    Another LTP template

--- pyiota/lattice/templates.py
+++ pyiota/lattice/templates.py
@@ -197,6 +197,99 @@ def LTP():
 
     return ltp
 
+class LTP2:
+    def __init__(self):
+        """ Based on injRingMatch1 example """
+        elements = {}
+
+        elements["LTP:PH"] = Monitor()
+        elements["LTP:PH1"] = Monitor()
+        elements["LTP:PH2"] = Monitor()
+        elements["LTP:PH3"] = Monitor()
+        elements["LTP:PH4"] = Monitor()
+        elements["LTP:PV"] = Monitor()
+        elements["LTP:PV1"] = Monitor()
+        elements["LTP:PV2"] = Monitor()
+        elements["LTP:PV3"] = Monitor()
+        elements["LTP:PV4"] = Monitor()
+
+        elements["LTP:H"] = Hcor(l=0.06)
+        elements["LTP:H1"] = Hcor(l=0.06)
+        elements["LTP:H2"] = Hcor(l=0.06)
+        elements["LTP:H3"] = Hcor(l=0.06)
+        elements["LTP:H4"] = Hcor(l=0.06)
+        elements["LTP:V"] = Vcor(l=0.06)
+        elements["LTP:V1"] = Vcor(l=0.06)
+        elements["LTP:V2"] = Vcor(l=0.06)
+        elements["LTP:V3"] = Vcor(l=0.06)
+        elements["LTP:V4"] = Vcor(l=0.06)
+        elements["LTP:HV"] = Vcor(l=0.06)
+
+        elements["LTPLMQ"] = Drift(l=0.18739)
+        elements["LTPLMQ1"] = Drift(l=0.26603)
+        elements["LTPLQC"] = Drift(l=0.12017)
+        elements["LTPLQ5C"] = Drift(l=0.19867)
+        elements["LTPLQC1"] = Drift(l=0.10525)
+        elements["LTPL12"] = Drift(l=1.926950)
+        elements["LTPL11"] = Drift(l=2.173730)
+        elements["LTPL10"] = Drift(l=2.413060)
+        elements["LTPL9"] = Drift(l=1.9465100)
+        elements["LTPL8A"] = Drift(l=1.462329)
+        elements["LTPL8B"] = Drift(l=0.20099)
+        elements["LTPL7"] = Drift(l=0.59678)
+        elements["LTPL6"] = Drift(l=0.70942)
+        elements["LTPL5"] = Drift(l=0.62634)
+        elements["LTPL4"] = Drift(l=0.62634)
+        elements["LTPL3"] = Drift(l=0.62634)
+        elements["LTPL3A"] = Drift(l=0.42535)
+        elements["LTPL3B"] = Drift(l=0.20099)
+        elements["LTPL2A"] = Drift(l=0.3317)
+        elements["LTPL2B"] = Drift(l=0.496950)
+        elements["LTPL1"] = Drift(l=2.904756)
+        elements["LTPL1A"] = Drift(l=1.345630)
+        elements["LTPL1B"] = Drift(l=0.7262)
+        elements["LTPL1C"] = Drift(l=0.15240)
+        elements["LTPL1D"] = Drift(l=0.677476)
+
+        elements["LTP:Q10"] = Quadrupole(l=0.3061, k1=1.32123855914)
+        elements["LTP:Q9"] = Quadrupole(l=0.3061, k1=-1.429201568445)
+        elements["LTP:Q8"] = Quadrupole(l=0.3061, k1=1.65302041674)
+        elements["LTP:Q7"] = Quadrupole(l=0.3061, k1=-1.782715766623)
+        elements["LTP:Q6"] = Quadrupole(l=0.3061, k1=2.927638995223)
+        elements["LTP:Q5"] = Quadrupole(l=0.1491, k1=-7.015653483830028)
+        elements["LTP:Q4"] = Quadrupole(l=0.3061, k1=4.908184713491)
+        elements["LTP:Q3"] = Quadrupole(l=0.3061, k1=-3.820413232879)
+        elements["LTP:Q2"] = Quadrupole(l=0.3061, k1=3.632979368008)
+        elements["LTP:Q1"] = Quadrupole(l=0.3061, k1=-0.934293465253)
+        elements["LTP:B1"] = SBend(l=0.4, angle=-0.2, e1=-0.1, e2=-0.1, gap=0.02 * 2, fint=0.5)
+        elements["LTP:SP"] = SBend(l=0.4, angle=0.2, e1=0.2, e2=0.0, gap=0.01 * 2, fint=0.5)
+
+        elements["LTP:FL1"] = Marker()
+        elements["LTP:FL2"] = Marker()
+        elements["LTP:FL3"] = Marker()
+        elements["LTP:CM1"] = Marker()
+        elements["LTP:END"] = Marker()
+
+        ltp1 = ["LTP:HV", "LTPL12", "LTP:Q10", "LTPLQC", "LTP:V4", "LTPL11",
+                "LTP:Q9", "LTPLQC", "LTP:H4", "LTPL10",
+                "LTP:Q8", "LTPL9", "LTP:PV4", "LTPLMQ", "LTP:Q7", "LTPLQC", "LTP:V3",
+                "LTPL8A", "LTP:FL3", "LTPL8B", "LTP:PH4", "LTPLMQ", "LTP:Q6", "LTPLQC",
+                "LTP:H3", "LTPL7", "LTP:B1"]
+        ltp2 = ["LTPL6", "LTP:PV3", "LTPLMQ1", "LTP:Q5", "LTPLQ5C", "LTP:V2",
+                "LTPL5", "LTP:PH3", "LTPLMQ", "LTP:Q4", "LTPLQC", "LTP:H2", "LTPL4",
+                "LTP:PV2", "LTPLMQ", "LTP:Q3", "LTPLQC", "LTP:V1", "LTPL3A", "LTP:FL2",
+                "LTPL3B", "LTP:PH2", "LTPLMQ", "LTP:Q2", "LTPLQC1", "LTP:H1",
+                "LTPL2A", "LTP:CM1", "LTPL2B", "LTP:Q1", "LTPL1A", "LTP:FL1", "LTPL1B",
+                "LTP:PH1", "LTPL1C", "LTP:PV1", "LTPL1D", "LTP:SP", "LTP:END"]
+        self.elements = elements
+        self.ltp1 = ltp1
+        self.ltp2 = ltp2
+        self.sequence = ltp1 + ltp2
+    # ltp1elems = [elements[eid] for eid in ltp1]
+    # ltp2elems = [elements[eid] for eid in ltp2]
+    # ltp = ltp1elems + ltp2elems
+
+
 
 def LTP_props():
     tw0 = Twiss()
