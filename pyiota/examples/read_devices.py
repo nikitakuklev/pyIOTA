415d72780e77d58ac3fcb5f503c168a3868cc66b
nikitakuklev <spontaneouscoder@gmail.com>
2023-11-20 07:58:55 -0600

    Update examples

--- pyiota/examples/read_devices.py
+++ pyiota/examples/read_devices.py
@@ -4,6 +4,6 @@ import pyIOTA.iota as iota
 devices = [acnet.frontends.DoubleDevice(b) for b in iota.QUADS.ALL_CURRENTS]
 ds = acnet.frontends.DeviceSet(name='bpms', members=devices)
 ds.adapter = acnet.frontends.ACNETRelay()
-ds.readonce()
+ds.read()
 
 print([[d.name, d.value_string] for d in devices])
